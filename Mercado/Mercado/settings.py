b7a61b7ab6f390e7ac42cbc6862edb1b62aed3a2
Walter Liendo <walter.liendo@gmail.com>
2022-08-19 16:07:49 -0300

    migracion

--- Mercado/Mercado/settings.py
+++ Mercado/Mercado/settings.py
@@ -121,4 +121,9 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = '/static/'
+
+AUTH_USER_MODEL="MercadoApp.CustomUser"
+
