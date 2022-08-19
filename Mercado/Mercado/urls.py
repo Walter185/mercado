a162167873f4a2f622b011d6f16f244651ec4203
Walter Liendo <walter.liendo@gmail.com>
2022-08-19 17:50:07 -0300

    temp

--- Mercado/Mercado/urls.py
+++ Mercado/Mercado/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from MercadoApp import views
 
-urlpatterns = [
+urlpatterns = [    
     path('admin/', admin.site.urls),
+    path('demo', views.demoPage),
+    path('demoPage', views.demoPageTemplate)
 ]
