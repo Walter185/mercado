3d14edac6ef13de1ed161f69c01db5381536ae57
Walter Liendo <walter.liendo@gmail.com>
2022-08-20 11:33:20 -0300

    merchant

--- Mercado/Mercado/urls.py
+++ Mercado/Mercado/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os import name
 from django.contrib import admin
 from django.urls import path
 from Mercado import settings
@@ -29,5 +30,17 @@ urlpatterns = [
     path('admin_login_process', views.adminLoginProcess,name="admin_login_process"),    
     path('admin_logout_process', views.adminLogoutProcess,name="admin_logout_process"),
 
-    path('admin_home',AdminViews.admin_home,name="admin_home")
+    path('admin_home',AdminViews.admin_home,name="admin_home"),
+
+    path('category_list',AdminViews.CategoriesListView.as_view(),name="category_list"),    
+    path('category_create',AdminViews.CategoriesCreate.as_view(),name="category_create"),
+    path('category_update/<slug:pk>',AdminViews.CategoriesUpdate.as_view(),name="category_update"),
+
+    path('sub_category_list',AdminViews.SubCategoriesListView.as_view(),name="sub_category_list"),
+    path('sub_category_create',AdminViews.SubCategoriesCreate.as_view(),name="sub_category_create"),
+    path('sub_category_update/<slug:pk>',AdminViews.SubCategoriesUpdate.as_view(),name="sub_category_update"),
+
+    path('merchant_create',AdminViews.MerchantUserCreateView.as_view(),name="merchant_create"),
+    path('merchant_list',AdminViews.MerchantUserListView.as_view(),name="merchant_list"),
+    path('merchant_update/<slug:pk>',AdminViews.MerchantUserUpdateView.as_view(),name="merchant_update"),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
